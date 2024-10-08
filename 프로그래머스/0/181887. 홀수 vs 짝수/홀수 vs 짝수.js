dd86e1c2a50c7454a7731f4afc258d4e20e38942
도은 <163129697+ehdms42@users.noreply.github.com>
2024-10-08 14:52:56 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,17 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+function solution(num_list) {
+    const sum = (condition) => num_list.filter((_, index) => condition(index)).reduce((acc, cur) => acc + cur, 0);
+
+    const oddSum = sum((index) => index % 2 === 0);
+    const evenSum = sum((index) => index % 2 === 1);
+
+    return oddSum < evenSum ? evenSum : oddSum;
+}
