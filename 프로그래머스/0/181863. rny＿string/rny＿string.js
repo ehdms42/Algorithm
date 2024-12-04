66ae0bc8cc34e2ba1116fc416d0f4531b37b266e
도은 <163129697+ehdms42@users.noreply.github.com>
2024-12-04 09:03:42 +0900

    [level 0] Title: rny_string, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.js
+++ 프로그래머스/0/181863. rny＿string/rny＿string.js
@@ -1,7 +1,3 @@
 function solution(rny_string) {
-    var answer = '';
-    for(let i = 0; i<=rny_string.length; i++){
-    answer = rny_string.replaceAll('m', 'rn')
-    }
-    return answer;
+    return rny_string.replaceAll('m', 'rn')
 }
