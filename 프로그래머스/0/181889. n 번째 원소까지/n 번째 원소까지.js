7661cce7b9f548ee267c3919f27fef7f843c45cc
도은 <163129697+ehdms42@users.noreply.github.com>
2024-12-08 14:21:38 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -1,8 +1,3 @@
 function solution(num_list, n) {
-     answer = [];
-    for(i = 0; i < num_list.length; i++) {
-        if(i < n) {
-            answer.push(num_list[i])
-        }
-    } return answer;
+    return num_list.slice(0,n);
 }
