5d718dddfd6130bdb2b4bf58ad8ade7cb04b95ee
도은 <163129697+ehdms42@users.noreply.github.com>
2025-08-10 11:26:16 +0900

    [level 0] Title: 5명씩, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.js
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -1,8 +1,3 @@
 function solution(names) {
-    var answer = [];
-    for(let i = 0; i<names.length; i+=5){
-    let temp = names.slice(i, i+5)
-        answer.push(temp[0])
-    }
-    return answer;
+    return names.filter((_, index) => index % 5 === 0);
 }
