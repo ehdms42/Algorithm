a93b45fc629e6483202ff41dfb14e4d8f5b6c079
도은 <163129697+ehdms42@users.noreply.github.com>
2025-08-30 11:42:46 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,16 +1,3 @@
 function solution(my_string, is_prefix) {
-    let count = 0;
-    const master = my_string.split("");
-    const prefix = is_prefix.split("");
-    if(master[0] === prefix[0]){
-        for(let i = 0; i < prefix.length; i++){
-            if(master[i] === prefix[i]){
-                count++;
-            }
-        }
-        if(prefix.length == count){
-            return 1;
-        }
-    }
-    return 0;
+    return my_string.startsWith(is_prefix) ? 1 : 0;
 }
