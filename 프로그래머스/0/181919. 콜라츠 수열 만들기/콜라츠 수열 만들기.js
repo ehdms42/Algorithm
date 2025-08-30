7cc81b2e53ff157bbbb6addac8cdf1bfffd24085
도은 <163129697+ehdms42@users.noreply.github.com>
2025-08-30 12:09:01 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.13 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,11 +1,11 @@
 function solution(n) {
-    let answer = [];
-    answer.push(n);
-    while(n > 1){
-        if(n % 2 == 0){
+    let answer = [n];
+    while (n !== 1) {
+        if (n % 2 === 0) {
             n = n / 2;
+        } else {
+            n = 3 * n + 1;
         }
-        else n = 3 * n + 1;
         answer.push(n);
     }
     return answer;
