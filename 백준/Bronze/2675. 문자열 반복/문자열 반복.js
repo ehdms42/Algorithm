b068d81647154c030440e57351f65b51e5c3043a
도은 <163129697+ehdms42@users.noreply.github.com>
2025-10-23 14:59:32 +0900

    [Bronze II] Title: 문자열 반복, Time: 92 ms, Memory: 9372 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let t = Number(input[0]);
+
+for (let i = 1; i <= t; i++) {
+  let [r, s] = input[i].split(' ');
+  r = Number(r);
+
+  let answer = s.split('').map(v => v.repeat(r)).join('');
+  console.log(answer);
+}
