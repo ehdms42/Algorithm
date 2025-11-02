f3659740e95b2eb697ab671c70059c838cd5165a
도은 <163129697+ehdms42@users.noreply.github.com>
2025-11-02 23:39:48 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -1,10 +1,6 @@
 function solution(a, b) {
-    const concat = parseInt(`${a}${b}`);
-    const mul = 2 * a * b;
-    
-    if (concat >= mul) {
-        return concat;
-    } else {
-        return mul;
-    }
+  const ab = Number(`${a}${b}`);
+  const result = 2 * a * b;
+
+  return ab >= result ? ab : result;
 }
