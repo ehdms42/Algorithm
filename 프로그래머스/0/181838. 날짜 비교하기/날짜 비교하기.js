1f73fe08e9aba2cd1e1cae4b597cfb4aeb9f6119
도은 <163129697+ehdms42@users.noreply.github.com>
2025-11-04 19:08:38 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,16 +1,3 @@
 function solution(date1, date2) {
-    // 연도를 비교
-    if (date1[0] < date2[0]) return 1;
-    if (date1[0] > date2[0]) return 0;
-
-    // 월을 비교
-    if (date1[1] < date2[1]) return 1;
-    if (date1[1] > date2[1]) return 0;
-
-    // 일을 비교
-    if (date1[2] < date2[2]) return 1;
-    if (date1[2] > date2[2]) return 0;
-
-    // 날짜가 같은 경우
-    return 0;
+    return Number(date1.join('')) < Number(date2.join('')) ? 1 : 0
 }
