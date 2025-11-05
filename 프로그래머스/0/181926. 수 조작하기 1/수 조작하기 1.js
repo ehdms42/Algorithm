e121538254f5097c7ee8f179f5e3d483a0617a94
도은 <qwaszx080402@naver.com>
2025-11-05 09:58:50 +0900

    [level 0] Title: 수 조작하기 1, Time: 5.95 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,14 +1,23 @@
 function solution(n, control) {
-    for (let str of control) {
-        if (str === 'w') {
-            n += 1; 
-        } else if (str === 's') {
-            n -= 1; 
-        } else if (str === 'd') {
-            n += 10; 
-        } else if (str === 'a') {
-            n -= 10; 
+
+    let controlSplit = control.split('');
+
+    for (let i = 0; i < controlSplit.length; i++) {
+        switch(controlSplit[i]) {
+            case "w":
+                n += 1;
+                break;
+            case "s":
+                n -= 1;
+                break;
+            case "d":
+                n += 10;
+                break;
+            case "a":
+                n -= 10;
+                break;
         }
     }
-    return n; 
+
+    return n;
 }
