3614b9304a6ff78b3e0e09999fcd0baadbe3e9f3
도은 <qwaszx080402@naver.com>
2025-11-07 17:28:12 +0900

    [level 0] Title: 카운트 업, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.js
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -1,7 +1,9 @@
 function solution(start_num, end_num) {
     let answer = [];
-    for(let i = start_num; i <= end_num; i++) {
+    
+    for(let i = start_num; i <= end_num; i++){
         answer.push(i);
     }
+    
     return answer;
 }
