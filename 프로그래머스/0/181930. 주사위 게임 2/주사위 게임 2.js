018d55df735a1159c269573c1dfb8be4fe2c034b
도은 <qwaszx080402@naver.com>
2025-11-09 12:05:25 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,5 +1,9 @@
 function solution(a, b, c) {
-    if(a === b && b === c && a === c) return (a + b + c) * (a**2 + b**2 + c**2 ) * (a**3 + b**3 + c**3)
-    if(a === b || b === c || a=== c) return (a + b + c) * (a**2 + b**2 + c**2)
-    return a+b+c
+  const sum = a + b + c;
+  const sum2 = a ** 2 + b ** 2 + c ** 2;
+  const sum3 = a ** 3 + b ** 3 + c ** 3;
+
+  if (a === b && b === c) return sum * sum2 * sum3;
+  if (a === b || a === c || b === c) return sum * sum2;
+  return sum;
 }
