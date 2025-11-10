fc41a47d2fd8f0e87fdc76c0570373eb44dd498b
도은 <qwaszx080402@naver.com>
2025-11-10 17:27:40 +0900

    [Bronze V] Title: A+B - 4, Time: 100 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,8 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+for (let i = 0; i < input.length; i++) {
+  let [a, b] = input[i].split(' ').map(Number);
+  console.log(a + b);
+}
+
