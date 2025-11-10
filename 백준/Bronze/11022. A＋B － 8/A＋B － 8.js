91d53235379edf68894b95e4a746d369e1f69c04
도은 <qwaszx080402@naver.com>
2025-11-10 19:01:28 +0900

    [Bronze V] Title: A+B - 8, Time: 92 ms, Memory: 9548 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,8 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let T = Number(input[0]);
+for (let i = 1; i <= T; i++) {
+    let [A, B] = input[i].split(' ').map(Number);
+    console.log(`Case #${i}: ${A} + ${B} = ${A + B}`);
+}
