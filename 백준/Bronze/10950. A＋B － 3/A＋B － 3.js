e42432f09ce4a9a17fde72cbcedb9e9576cbd186
도은 <qwaszx080402@naver.com>
2025-11-10 17:30:30 +0900

    [Bronze V] Title: A+B - 3, Time: 96 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let n = Number(input[0]);
+
+for (let i = 1; i <= n; i++) {
+  let [a, b] = input[i].split(' ').map(Number);
+  console.log(a + b);
+}
