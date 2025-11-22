fd2b6d00fd6b53f8def37355eb3dcdb70ff51eae
도은 <qwaszx080402@naver.com>
2025-11-22 21:15:40 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,7 +1,7 @@
 function solution(myString, pat) {
-    let temp = myString.replaceAll("A", "tmp");
-        temp = temp.replaceAll("B", "A");
-        temp = temp.replaceAll("tmp", "B");
-    
-    return temp.includes(pat) ? 1 : 0;
+    return myString
+        .replaceAll('A','x')  
+        .replaceAll('B', 'A') 
+        .replaceAll('x','B')  
+        .includes(pat) ? 1 : 0;
 }
