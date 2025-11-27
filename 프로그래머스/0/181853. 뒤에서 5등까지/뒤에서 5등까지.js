b54b8d336cc4c65fa65c255a714b46bb0d1e19fa
도은 <qwaszx080402@naver.com>
2025-11-27 10:56:36 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.js
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.js
@@ -1,6 +1,3 @@
 function solution(num_list) {
-    var answer = [];
-    arr = num_list.sort((a, b) => a - b);
-    answer = arr.slice(0, 5)
-    return answer;
+    return num_list.sort((a, b) => a - b).slice(0,5);
 }
