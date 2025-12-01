3819b8ebd097257951785947717137544f110181
도은 <qwaszx080402@naver.com>
2025-12-01 17:23:37 +0900

    [level 0] Title: 배열 비교하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -1,1 +1,10 @@
-const solution = (arr1, arr2) => arr1.length !== arr2.length ? (arr1.length > arr2.length ? 1 : -1) : (arr1.reduce((a, b) => a + b, 0) > arr2.reduce((a, b) => a + b, 0) ? 1 : (arr1.reduce((a, b) => a + b, 0) < arr2.reduce((a, b) => a + b, 0) ? -1 : 0));
+function solution(arr1, arr2) {
+    if (arr1.length !== arr2.length){
+        return arr1.length > arr2.length ? 1 : -1;
+    }
+    
+    const sum1 = arr1.reduce((acc, cur) => acc + cur, 0);
+    const sum2 = arr2.reduce((acc, cur) => acc + cur, 0);
+    
+    return sum1 === sum2 ? 0 : (sum1 > sum2 ? 1 : -1);
+}
