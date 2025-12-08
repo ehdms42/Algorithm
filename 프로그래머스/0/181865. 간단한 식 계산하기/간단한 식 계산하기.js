7b184405da5510f3cabd57f6041ef25fe3251da5
도은 <qwaszx080402@naver.com>
2025-12-08 13:38:10 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -1,16 +1,12 @@
 function solution(binomial) {
-    const parts = binomial.split(' ');
-
-    const a = parseInt(parts[0], 10);
-    const op = parts[1];
-    const b = parseInt(parts[2], 10);
-
-    switch (op) {
-        case '+':
-            return a + b;
-        case '-':
-            return a - b;
-        case '*':
-            return a * b;
+    let [aStr, op, bStr] = binomial.split(' ');
+    
+    let a = parseInt(aStr);
+    let b = parseInt(bStr);
+    
+    switch (op){
+        case '+' : return a + b;
+        case '-' : return a - b;
+        case '*' : return a * b;
     }
 }
