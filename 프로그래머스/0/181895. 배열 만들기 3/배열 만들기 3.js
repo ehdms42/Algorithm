2e1fe3a524025942ee805c83e96fb49c5fc476fb
도은 <qwaszx080402@naver.com>
2025-12-08 17:25:17 +0900

    [level 0] Title: 배열 만들기 3, Time: 4.11 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,11 +1,5 @@
 function solution(arr, intervals) {
-    let res = [];
+    const [[a,b],[c,d]] = intervals;
 
-    for (let [start, end] of intervals) {
-        for (let i = start; i <= end; i++) {
-            res.push(arr[i]);
-        }
-    }
-
-    return res;
+    return [...arr.slice(a, b+1), ...arr.slice(c, d+1)];
 }
