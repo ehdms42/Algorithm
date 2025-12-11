e61e300d0a0fa1b5e269a52ac64e84964e8c2528
도은 <qwaszx080402@naver.com>
2025-12-11 19:20:36 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
@@ -1,6 +1,3 @@
 function solution(num_list, n) {
-    const beforeN = num_list.slice(0, n); 
-    const afterN = num_list.slice(n);
-
-    return afterN.concat(beforeN);
+    return [...num_list.slice(n), ...num_list.slice(0, n)];
 }
